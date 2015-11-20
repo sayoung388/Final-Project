a8db7053dcb6ab0bc26a394e0b341f9d38451a1d
sayoung388 <sayoung388@gmail.com>
2015-11-19 22:51:27 -0700

    added main menu

--- MyApplication/app/src/main/java/com/example/syoung/final_project/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/syoung/final_project/MainActivity.java
@@ -1,21 +1,36 @@
 package com.example.syoung.final_project;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity implements OnItemClickListener {
+
+    private ListView mainListView;
+    private LinearLayout mainLinearLayout;
+    private String mainListArray[]={"Food Storage", "Budget", "Settings"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //mainLinearLayout = (LinearLayout)findViewById(R.id.mainMenuLayout);
+        mainListView = (ListView)findViewById(R.id.listView);
+        mainListView.setAdapter(new ArrayAdapter<String>(this, R.layout.menu_text, mainListArray));
+        mainListView.setOnItemClickListener(this);
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+                Toast.LENGTH_SHORT).show();
+    }
 }
